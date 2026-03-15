879c4769e94d13f2e4782882a11a4b7dfbf3d8b7
rheannemcintosh <rheannemcintosh@gmail.com>
2026-03-15 01:14:22 +0000

    Add shared Azure Table Storage client
    Creates a reusable TableClient instance connected to the concepts table,
    using the STORAGE_CONNECTION_STRING environment variable.
    
    [LLA-3]

---
+++ shared/tableClient.js
@@ -0,0 +1,8 @@
+const { TableClient } = require("@azure/data-tables");
+
+const client = TableClient.fromConnectionString(
+  process.env.STORAGE_CONNECTION_STRING,
+  "concepts"
+);
+
+module.exports = client;
